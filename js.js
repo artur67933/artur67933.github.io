fa8cb454673b977403be5062ca650f3c4839a71c
artur67933 <ahc.santos@campus.fct.unl.pt>
2026-09-05 02:38:44 +0100

    test 3

--- js.js
+++ js.js
@@ -1,7 +1,29 @@
 document.addEventListener ("DOMContentLoaded", handleDocumentLoad);
 
-function handleDocumentLoad() {
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
 
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function handleDocumentLoad() {
 //Variable
 var offSwitch = document.getElementById("lightSwitchOff"); //Targets div with ID lightSwitchOff
 var onSwitch = document.getElementById("lightSwitchOn"); //Targets div with ID lightSwitchOn
@@ -20,6 +42,7 @@ function lightsOff() { /*This changes the background colour to black and makes t
     onSwitch.innerHTML = "Turn Off Night Mode";
     onSwitch.style.display = "inline";
     offSwitch.style.display = "none";
+    setCookie("lights", "", 30);
 }
 
 function lightsOn() { /*This changes the background colour to a white and makes text black*/
@@ -27,5 +50,14 @@ function lightsOn() { /*This changes the background colour to a white and makes 
     offSwitch.innerHTML = "Turn On Night Mode";
     onSwitch.style.display = "none";
     offSwitch.style.display = "inline";
+    setCookie("lights", "on", 30);
 }
+
+
+if (getCookie("lights") == "on") {
+        lightsOn();
+    } else {
+        lightsOff();
+    }
+
 }
