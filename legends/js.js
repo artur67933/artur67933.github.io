3e7b5c695a1afc505e136a5b3f797f393d42c395
artur67933 <ahc.santos@campus.fct.unl.pt>
2026-09-06 17:11:45 +0100

    fix cookies

--- legends/js.js
+++ legends/js.js
@@ -1,8 +1,11 @@
 document.addEventListener ("DOMContentLoaded", handleDocumentLoad);
 
 function setCookie(cname, cvalue) {
+  var exdays = 365;
   const d = new Date();
-  document.cookie = cname + "=" + cvalue + ";path=/";
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 function getCookie(cname) {
